7235f04432c207c40e15ca24c3f6d72947e59901
Luminoso1 <novoaavila16gmail.com>
2026-04-06 20:56:57 -0500

    init

---
+++ src/routes/index.tsx
@@ -0,0 +1,7 @@
+import { createFileRoute } from '@tanstack/react-router'
+
+export const Route = createFileRoute('/')({ component: App })
+
+function App() {
+  return <main><h1>Hello there!!</h1></main>
+}
