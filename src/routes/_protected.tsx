14304bbd189ea20c7992fbd40a36139674420bf4
Luminoso1 <novoaavila16gmail.com>
2026-04-09 21:40:43 -0500

    feat: sign out handler added

--- src/routes/_protected.tsx
+++ src/routes/_protected.tsx
@@ -1,4 +1,5 @@
-import { createFileRoute, redirect } from '@tanstack/react-router'
+import { createFileRoute, redirect, Outlet, Link } from '@tanstack/react-router'
+import { signOut } from '#/lib/auth/authClient'
 
 export const Route = createFileRoute('/_protected')({
   beforeLoad: async ({ context }) => {
@@ -10,5 +11,35 @@ export const Route = createFileRoute('/_protected')({
 })
 
 function RouteComponent() {
-  return <div>Hello "/_protected"!</div>
+  const navigate = Route.useNavigate()
+  const handleSignOut = () => {
+    signOut({
+      fetchOptions: {
+        onSuccess: () => {
+          navigate({ to: '/login' })
+        },
+      },
+    })
+  }
+
+  return (
+    <div className="max-w-7xl mx-auto">
+      <header>
+        <div className="py-4 flex items-center justify-between">
+          <menu className="flex items-center gap-8 py-6">
+            <Link to="/dashboard">Dashboard</Link>
+            <Link to="/config">Config</Link>
+          </menu>
+
+          <button
+            onClick={handleSignOut}
+            className="bg-black hover:bg-slate-800 dark:bg-white dark:text-black dark:hover:bg-gray-200 px-8 py-3 text-xs font-bold uppercase tracking-widest transition-all shadow-2xl active:scale-95"
+          >
+            Sign Out
+          </button>
+        </div>
+      </header>
+      <Outlet />
+    </div>
+  )
 }
