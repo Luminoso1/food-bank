9861aa9135934230ebce9cf1f3d39d8f6ae70eab
Luminoso1 <novoaavila16gmail.com>
2026-04-12 13:36:23 -0500

    feat: add connection pooling to optimize performance and resources

--- src/db/index.ts
+++ src/db/index.ts
@@ -1,6 +1,26 @@
 import { drizzle } from 'drizzle-orm/node-postgres'
 import { env } from '#/env/server.ts'
+import { Pool } from 'pg'
 
 import * as schema from './schema.ts'
 
-export const db = drizzle(env.DATABASE_URL, { schema })
+const connectionString = env.DATABASE_URL
+
+const pool = new Pool({ connectionString })
+
+// --- Pool Logs ---
+
+//
+pool.on('connect', () => {
+  console.info('[DB] New connection created with PostgreSQL!')
+})
+
+pool.on('acquire', () => {
+  console.info('[DB] Connection acquired from Pool!')
+})
+
+pool.on('error', (err) => {
+  console.error('❌ [DB] Error inesperado en el pool', err)
+})
+
+export const db = drizzle(pool, { schema })
